9c7171fcf771ae0f839ff4022573d2a0b57aae1d
DAVOOD RABBANI <123607656+shaikdr1@users.noreply.github.com>
2024-08-07 22:26:41 -0500

    Update project 3.scala

--- project 3 (2).scala
+++ project 3 (2).scala
@@ -1,25 +1,4 @@
-// Databricks notebook source
-// MAGIC %md
-// MAGIC **Main Project (100 pts)** \
-// MAGIC Implement closed-form solution when m(number of examples is large) and n(number of features) is small:
-// MAGIC \\[ \scriptsize \mathbf{\theta=(X^TX)}^{-1}\mathbf{X^Ty}\\]
-// MAGIC Here, X is a distributed matrix.
 
-// COMMAND ----------
-
-// MAGIC %md
-// MAGIC Steps:
-// MAGIC 1. Create an example RDD for matrix X and vector y
-// MAGIC 2. Compute \\[ \scriptsize \mathbf{(X^TX)}\\]
-// MAGIC 3. Convert the result matrix to a Breeze Dense Matrix and compute pseudo-inverse
-// MAGIC 4. Compute \\[ \scriptsize \mathbf{X^Ty}\\] and convert it to Breeze Vector
-// MAGIC 5. Multiply \\[ \scriptsize \mathbf{(X^TX)}^{-1}\\] with \\[ \scriptsize \mathbf{X^Ty}\\]
-
-// COMMAND ----------
-
-
-
-// COMMAND ----------
 
 import org.apache.spark.mllib.linalg.{Vector, Vectors, Matrix}
 import org.apache.spark.mllib.linalg.distributed.RowMatrix
@@ -39,7 +18,6 @@ val re_dense = new breeze.linalg.DenseMatrix(XTX.numRows, XTX.numCols, XTX.toArr
 val inver = pinv(re_dense)
 
 
-// COMMAND ----------
 
 
 val M_Small = sc.parallelize(mat_data.zipWithIndex.flatMap{case (row, i) => row.zipWithIndex.map { case (value, j) => ((i.toInt, j.toInt), value)}})
@@ -54,28 +32,5 @@ val sdr = result.collect()
 val rac = sdr.map{ case (i, value) => (i, value) }.sortBy(_._1).map(_._2)
 val na = new DenseVector(rac)
 
-// COMMAND ----------
 
 val c = inver * na
-
-// COMMAND ----------
-
-// MAGIC %md
-// MAGIC Alternatively, you can implement \\[ \scriptsize \mathbf{\theta=(X^TX)}^{-1}\mathbf{X^Ty}\\] using Spark DataFrame
-
-// COMMAND ----------
-
-// MAGIC %md
-// MAGIC **Bonus 1(10 pts)** \
-// MAGIC Implement \\[ \scriptsize \mathbf{\theta=(X^TX)}^{-1}\mathbf{X^Ty}\\] where you compute \\[ \scriptsize \mathbf{\theta=(X^TX)}\\] using outer-product technique. You can use either Scala or DataFame to implement this.
-
-// COMMAND ----------
-
-// MAGIC %md
-// MAGIC
-
-// COMMAND ----------
-
-// MAGIC %md
-// MAGIC **Bonus 2(20 pts)** \
-// MAGIC Run your algorithm on Boston Housing Dataset: https://www.kaggle.com/datasets/vikrishnan/boston-house-prices?resource=download
